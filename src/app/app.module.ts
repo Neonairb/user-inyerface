8f5f2f6c01bd5832e1f6e775964b14199e1f0b75
Brian Acosta <brajai@hotmail.com>
2023-03-12 19:24:24 -0700

    Fixed Home Page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { APP_ROUTING } from './app.routes';
+
 import { AppComponent } from './app.component';
-import { InicioComponent } from './home/home.component';
 import { SessionComponent } from './session/session.component';
 import { PpInterestsComponent } from './pp-interests/pp-interests.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    InicioComponent,
     SessionComponent,
-    PpInterestsComponent
+    PpInterestsComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,10 @@
 import { RouterModule, Routes } from '@angular/router'
-import { InicioComponent } from './home/home.component';
+import { HomePageComponent } from './home-page/home-page.component';
 import { SessionComponent } from './session/session.component';
 
 
 const APP_ROUTES: Routes = [
-    { path: 'inicio', component: InicioComponent },
+    { path: 'inicio', component: HomePageComponent },
     { path: 'session', component: SessionComponent },
     { path: '**', pathMatch: 'full', redirectTo: 'inicio' }
 ];
